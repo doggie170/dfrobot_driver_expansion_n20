4c8f3ceee9cec6d628d2d411fcc63b6c23028d1d
doggie170 <76426140+doggie170@users.noreply.github.com>
2021-05-09 12:09:48 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    motor.motorStop(motor.Motors.M1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 197)
+})
 basic.forever(function () {
 	
 })
